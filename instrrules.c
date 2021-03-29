5deb2895bd3d7bf306a852ab8c0c7b2533ebae07
Will Angenent <will.angenent@gmail.com>
2021-03-29 19:51:33 +0200

    Added high cost rules to sign extend IR_ARG arguments

--- instrrules.c
+++ instrrules.c
@@ -1026,6 +1026,12 @@ void init_instruction_selection_rules() {
     r = add_rule(0, IR_RETURN, ADRV, 0, 1); add_op(r, X_RET,  0,   SRC1, 0, "movq %v1q, %%rax"  );              // Return a *void
 
     r = add_rule(0, IR_ARG, CSTL, CSTL, 2); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq $%v2q"); // Use constant as function arg, must not be a quad
+
+    // Add rules for sign extention an arg, but at a high cost, to encourage other rules to take precedence
+    r = add_rule(0, IR_ARG, CSTL, REGB, 10); add_op(r, X_MOVSBQ, SRC2, SRC2, 0 , "movsbq %v1b, %v1q"); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq %v2q" );
+    r = add_rule(0, IR_ARG, CSTL, REGW, 10); add_op(r, X_MOVSWQ, SRC2, SRC2, 0 , "movswq %v1w, %v1q"); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq %v2q" );
+    r = add_rule(0, IR_ARG, CSTL, REGL, 10); add_op(r, X_MOVSLQ, SRC2, SRC2, 0 , "movslq %v1l, %v1q"); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq %v2q" );
+
     r = add_rule(0, IR_ARG, CSTL, REGQ, 2); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq %v2q" ); // Use register as function arg, must be a quad
 
     r = add_rule(0, IR_ARG, CSTL, ADRB, 2); add_op(r, X_ARG, 0, SRC1, SRC2, "pushq %v2q" ); // Use pointer in register as function arg
