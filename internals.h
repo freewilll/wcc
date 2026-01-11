b034adfa29ac29b879e3e51feb55ba2ffa16ad47
Will Angenent <will.angenent@gmail.com>
2026-01-11 13:43:37 +0000

    Moved __builtin_va_list to be internal since it's incorrect to define this in the header ...
    ... musl stdarg.h relies on this

--- internals.h
+++ internals.h
@@ -9,3 +9,13 @@ struct __wcc_register_save_area {
 
 extern void *memcpy(void *s1, const void *s2, unsigned long n);
 extern void *memset(void *str, int c, unsigned long n);
+
+typedef struct __va_list {
+    unsigned int gp_offset;
+    unsigned int fp_offset;
+    void *overflow_arg_area;
+    void *reg_save_area;
+} va_list[1];
+
+typedef va_list __gnuc_va_list;
+typedef va_list __builtin_va_list;
